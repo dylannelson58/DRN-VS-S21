9ae391b498af27edabe53053f464862a445aa0c7
Dylan Nelson <dylannelson58@gmail.com>
2021-01-15 08:19:53 -0700

    add code

---
+++ Operators.vb
@@ -0,0 +1,12 @@
+﻿Module Operators
+
+    Sub Main()
+        Console.WriteLine(2 + 2)
+        Console.WriteLine(40 + 40)
+        Console.WriteLine(27 / 3)   'Regular division
+        Console.WriteLine(1 Mod 2)  'Integer division
+        Console.WriteLine(1 \ 2)    'Remainder only divison
+        Console.ReadLine()
+    End Sub
+
+End Module
